8a383e817d43f8465315b5a6a16ddf57ecd89fee
🤔Chul Hee Woo <wochul@amazon.com>
2020-07-04 18:10:13 -0700

    Profile DELETE API for deleting user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -134,4 +134,22 @@ router.get('/user/:user_id', async (req, res) => {
     }
 })
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  PPrivate
+router.delete('/', auth, async (req, res) => {
+
+    try {
+        // @todo - remove users posts
+
+        await Profile.findOneAndRemove({ user: req.user.id });
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
