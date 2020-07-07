7aecb52e04d0cc810cfdd92637cf37f794294d51
🤔Chul Hee Woo <wochul@amazon.com>
2020-07-06 21:18:44 -0700

    loadUser action match token in localStorage to backend GET /api/auth API

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,6 +1,28 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { REGISTER_SUCCESS, REGISTER_FAIL } from "./types";
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from "./types";
+import setAuthTokenHeader from "../utils/setAuthTokenHeader";
+
+// Load User
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    // sets the header with token, if one exists
+    setAuthTokenHeader(localStorage.token);
+  }
+
+  try {
+    const res = await axios.get("/api/auth");
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: AUTH_ERROR,
+    });
+  }
+};
 
 // Register User
 export const register = ({ name, email, password }) => async (dispatch) => {
