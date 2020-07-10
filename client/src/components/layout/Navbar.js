ffc20cd1ee77fe09e07944db6774dacc7208408e
🤔Chul Hee Woo <wochul@amazon.com>
2020-07-09 23:49:03 -0700

    Starting on Dashboard - added link to create-profile API if user does not have one
    * Also added CLEAR_PROFILE action type to clear reset profile to null upon logout.
    * Added Spinner when loading Dashboard
    * Redirected Landing to Dashboard if the user is already logged in.

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,15 +1,22 @@
-import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { logout } from "../../actions/auth";
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
 export const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" /> <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href="#!">
-          <i className="fas fa-sign-out-alt"></i> <span className="hide-sm">Logout</span>
+          <i className="fas fa-sign-out-alt" />
+          {''}
+          <span className="hide-sm">Logout</span>
         </a>
       </li>
     </ul>
